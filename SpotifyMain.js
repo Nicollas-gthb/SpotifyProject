9db4eb0e3ce4e8f260b8337b6ad4faae716ae6ce
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-01-11 23:21:38 -0300

    feat: botão play troca de icone ao tocar

---
+++ SpotifyMain.js
@@ -0,0 +1,25 @@
+const audio = document.querySelector(".audio-player");
+
+const playPauseButton = document.querySelector(".controladorCima-circulo");
+const playPause = document.querySelector(".controladorCima-play");
+
+const playIcon = "icons/playbutton.svg";
+const pauseIcon = "icons/pause-icon.svg";
+
+const progressRange = document.querySelector(".controladorBaixo-progresso");
+
+const currentTime = document.querySelector(".controladorBaixo-inicio");
+const totalTime = document.querySelector(".controladorBaixo-fim");
+
+
+playPauseButton.addEventListener('click', () => {
+    if(audio.paused){
+        audio.play();
+        playPause.src = pauseIcon;
+        playPause.style.marginLeft = "0px";
+    }else{
+        audio.pause();
+        playPause.src = playIcon;
+        playPause.style.marginLeft = "4px";
+    }
+})
